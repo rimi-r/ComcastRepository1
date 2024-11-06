0f579788d3cbf4791131ee145fb2329548b0d9ed
rstar <rstar@Risp>
2024-11-06 11:13:07 +0530

    new printing statement

--- GitHubFramework11/src/test/java/com/demoTest/DemoTest1.java
+++ GitHubFramework11/src/test/java/com/demoTest/DemoTest1.java
@@ -13,6 +13,8 @@ public class DemoTest1 {
 			System.out.println("test1");
 			System.out.println("test3");
 			System.out.println("test4");
+			System.out.println("test5");
+			System.out.println("test6");
 
 		}
 		
