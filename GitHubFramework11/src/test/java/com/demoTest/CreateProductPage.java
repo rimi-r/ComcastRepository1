7c602c27bc9a78588d15b7590b5c7d7445fe172a
LENOVO <LENOVO@DESKTOP-BL08O2D>
2024-11-06 17:54:58 +0530

    added new class to local branch

---
+++ GitHubFramework11/src/test/java/com/demoTest/CreateProductPage.java
@@ -0,0 +1,11 @@
+package com.demoTest;
+
+import org.testng.annotations.Test;
+
+public class CreateProductPage {
+
+	@Test
+	public void createProduct() {
+		System.out.println("add product");
+	}
+}
